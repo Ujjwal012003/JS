d8e5e92e86ed76ffba9605c91a69baec16dd639d
Ujjwal012003 <ujjwaal05@gmail.com>
2025-02-17 05:01:56 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,31 @@
+// Primitive
+
+//7 types: String, number, boolean, null, undefined, symbol, Bigint
+
+const score = 100
+const scoreValue=100.5
+
+const isLoggedIn = false
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 3434455743556n
+
+//Reference type (Non primitive)
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman","naagraj","doga"]
+let myObj={
+    name: "Ujjwal",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("Hello world")
+}
